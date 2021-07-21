afa9345196c7c2195ad6c5dead03aa97dcf0a303
willr <willr@CLOUDS-PC>
2021-07-21 14:15:50 -0400

    Add project files.

---
+++ 01_MyFirstProgram/Main.cpp
@@ -0,0 +1,7 @@
+//This is my first C++ Program!
+#include <iostream>
+int main() 
+{
+	std::cout << "Hi there!\nThis is my very first program!\nI am trying to teach myself C++ to serve clients!";
+	return 0;
+}
