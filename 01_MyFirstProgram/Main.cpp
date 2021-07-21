1fa17eea77ba735f7fff770fc2579928ce958b94
cloud peacock <willrpeacock@gmail.com>
2021-07-21 14:38:44 -0400

    Made it to where the application pauses for 5 seconds before closing!

--- 01_MyFirstProgram/Main.cpp
+++ 01_MyFirstProgram/Main.cpp
@@ -1,7 +1,14 @@
-//This is my first C++ Program!
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 #include <iostream>
+#include <cstdlib>
+using namespace std;
 int main() 
 {
-	std::cout << "Hi there!\nThis is my very first program!\nI am trying to teach myself C++ to serve clients!";
+	cout << "Hi there!\nThis is my very first program!\nI am trying to teach myself C++ to serve clients!";
+	Sleep(5000);
 	return 0;
 }
